fd245d31a34a1f931771d7f9e0310a6404cc9ce9
Dilnuwan Weerasuriya <dilnuwanweerasuriya@gmail.com>
2025-12-23 15:47:29 +0530

    New Updates after magazine 3 release

--- src/data/magazineData.js
+++ src/data/magazineData.js
@@ -3,18 +3,18 @@ export const magazineData = [
         id: 1,
         volume: 1,
         image: "/images/magazine/v1.webp",
-        date: "9 October, 2025"
+        date: "16 July, 2025"
     },
     {
         id: 2,
         volume: 2,
         image: "/images/magazine/v2.webp",
-        date: "16 November, 2025"
+        date: "1 October, 2025"
     },
     {
         id: 3,
         volume: 3,
         image: "/images/magazine/v3.webp",
-        date: "Coming Soon"
+        date: "22 December, 2025"
     },
 ]
